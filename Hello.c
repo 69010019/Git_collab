d270d0fff09fbed34409426da750b41041f4ad13
Developer B <69010216@kmitl.ac.th>
2026-09-23 17:56:07 +0700

    Add farewell Function and edit dmain Function

--- Hello.c
+++ Hello.c
@@ -6,8 +6,16 @@ int greeting(const char *name)
     printf("Hello, %s!\n", name);
     return 0;
 }
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
+    return 0;
+}
+
 int main(void)
 {
-    greeting("Developer A, B, C !!");
+    greeting("Developer A,B,C !!");
+    farewell("Developer A,B,C");
     return 0;
 }
